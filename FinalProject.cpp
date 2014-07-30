6e2535de5af11274c6a966c5ddc8435074db1b2d
Tyson <tyson518@users.noreply.github.com>
2014-07-29 19:06:52 -0600

    Forgot to add the FinalProject.cpp

---
+++ FinalProject.cpp
@@ -0,0 +1,93 @@
+// FinalProj'ect.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "Library.h"
+
+using namespace std;
+
+Artist* addMenu();
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	/*
+	Menu Commands:
+	Add - Adds album to the Library
+	Search - Finds all Artists, Albums, and Songs that match the specified value
+	Delete - Removed specified Artist, Album, or Song
+	Print - Print specifed Artist, Album, or Song
+	PrintLib - Print the entire library to the screen
+	Help - Print help menu
+	LoadLib - Load Library from a file
+	SaveLib - Save Library to a file
+	Quit - Exit
+	*/
+
+	Library *lib = new Library();
+
+	string command = "";
+	while(command.compare("Quit") != 0){
+		cout << "Enter Command: ";
+		cin >> command;
+		if (command.compare("Add") == 0){
+			Artist *newArtist = addMenu();
+			//(*lib).add(newArtist); // TODO implement
+		} else if (command.compare("Search") == 0){
+			cout << "Search Menu";
+		} else if (command.compare("Delete") == 0){
+			cout << "Delete Menu";
+		} else if (command.compare("Print") == 0){
+			cout << "Print Search results Menu";
+		} else if (command.compare("PrintLib") == 0){
+			(*lib).print();
+		} else if (command.compare("Help") == 0){
+			cout << "Add Menu";
+		} else if (command.compare("LoadLib") == 0){
+			(*lib).loadLibrary();
+			cout << "Library loaded from the file.";
+		} else if (command.compare("SaveLib") == 0){
+			(*lib).saveLibrary();
+			cout << "Library saved to the file.";
+		}
+		cout << endl;
+	}
+
+	(*lib).deleteLibrary();
+
+	system("pause");
+	return 0;
+}
+
+Artist* addMenu(){
+	Artist *newArtist;
+	string artist, album;
+	cout << "Please enter an artist: " << endl;
+    cin >> artist;
+	newArtist = new Artist(artist);
+    cout << "Please enter first album name" << endl;
+    cin >> album;
+    while (album.compare("N") != 0) {
+        cout << "How many songs on this album?" << endl;
+        int numSongs;
+        cin >> numSongs;
+        Song **songs;
+		songs = new Song*[numSongs]; //songs name
+
+        for (int i = 0; i < numSongs; i++) {
+            cout << "enter song length: " << i << endl;
+            int length;
+            cin >> length;
+            cout << "enter song name: " << i << endl;
+            string name;
+            cin >> name;
+            songs[i] = new Song(name, length);
+        }
+        (*newArtist).addAlbum(new Album(album, numSongs, songs));
+
+        cout << "Please enter next album name or N if done" << endl;
+        cin >> album;
+    }
+
+	return newArtist;
+}
